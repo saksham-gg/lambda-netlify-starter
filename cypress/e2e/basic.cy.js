fa7276b0d5a5197eeabf557c3b2938116de29327
Saksham Garg <106537109+saksham-gg@users.noreply.github.com>
2024-02-16 13:49:39 +0530

    test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,7 +5,7 @@ describe('sample test', () => {
 
   it('displays the resources text', () => {
     cy.get('h1')
-    .contains('Welcome to my app today!');
+    .contains('Welcome to my app today again!');
   })
   it('renders the Netlify logo image', () => {
     cy.get('img')
